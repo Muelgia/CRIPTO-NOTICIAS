b56ca36d8ec05786004cd93405dfb2e54844c597
MuelGia <samuelcarlosgia@gmail.com>
2025-01-11 18:45:17 -0300

    250111 - Algumas melhorias

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
+driver.close()
+
 # para cada noticia que foi adicionada na lista, entra no site e pega todos os <p>
 for link in noticias:
     try:
